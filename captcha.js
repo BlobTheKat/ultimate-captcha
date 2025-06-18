418d396cab82064bac84745f4dee8806cee8126b
BlobTheKat <blob.kat@hotmail.com>
2025-06-19 00:47:30 +0100

    multi-round to make 🤢pple-friendly

--- captcha.js
+++ captcha.js
@@ -2,13 +2,19 @@ export function Captcha(challenge){
 	const t0 = performance.now()
 	const canvas = document.createElement('canvas')
 	const gl = canvas.getContext('webgl2')
+	canvas.addEventListener('webglcontextlost', e => alert('webgl lost'))
 	const tex = gl.createTexture()
 	gl.bindTexture(gl.TEXTURE_2D_ARRAY, tex)
 	gl.texStorage3D(gl.TEXTURE_2D_ARRAY, 1, gl.RGBA32UI, 256, 256, 256)
-	const buf = new Uint32Array(1<<26)
-	for(let i = buf.length; (i-=16384) >= 0;)
-		crypto.getRandomValues(buf.subarray(i, i+16384))
-	gl.texSubImage3D(gl.TEXTURE_2D_ARRAY, 0, 0, 0, 0, 256, 256, 256, gl.RGBA_INTEGER, gl.UNSIGNED_INT, buf)
+	const heap = new Uint32Array(1<<26)
+	let xsh = Math.floor(Math.random()*4294967296)|0
+	for(let i = 0; i < (1<<26); i++){
+		heap[i] = xsh;
+		xsh ^= xsh << 13;
+		xsh ^= xsh >> 17;
+		xsh ^= xsh << 5;
+	}
+	gl.texSubImage3D(gl.TEXTURE_2D_ARRAY, 0, 0, 0, 0, 256, 256, 256, gl.RGBA_INTEGER, gl.UNSIGNED_INT, heap)
 	gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
 	gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	const v = gl.createShader(gl.VERTEX_SHADER), f = gl.createShader(gl.FRAGMENT_SHADER)
@@ -16,19 +22,22 @@ export function Captcha(challenge){
 	gl.shaderSource(v, `#version 300 es\nvoid main(){gl_Position=vec4(float(gl_VertexID-1)*2.0625,float((gl_VertexID<<2&4)-1),0.,1.);}`)
 	gl.compileShader(v)
 	gl.shaderSource(f, `#version 300 es
-precision highp int; precision highp usampler2DArray;
+precision highp int; precision highp usampler2DArray; precision highp usampler2D;
 uniform usampler2DArray heap;
-uniform int iters;
-out uint ret;
-uint kernel(uint);
-void main(){ ret = kernel(uint(gl_FragCoord.x)|uint(gl_FragCoord.y)<<4); }
-uint kernel(uint id){
-	uint ret = 0u;
-	for(int i=0;i<65536;i++){
+uniform usampler2D last;
+out uvec2 ret;
+void kernel(inout uvec2);
+void main(){
+	ret = texelFetch(last, ivec2(gl_FragCoord.xy), 0).xy;
+	kernel(ret);
+}
+void kernel(inout uvec2 state){
+	uint id = state.y;
+	for(int i=0;i<4096;i++){
 		id = texelFetch(heap, ivec3(id>>2&255u, id>>10&255u, id>>18&255u), 0)[id&3u];
-		ret ^= id;
+		state.x ^= id;
 	}
-	return ret;
+	state.y = id;
 }`)
 	gl.compileShader(f)
 	let err = gl.getShaderInfoLog(f)
@@ -38,19 +47,34 @@ uint kernel(uint id){
 	err = gl.getProgramInfoLog(p)
 	if(err) console.warn(err)
 	gl.useProgram(p)
-	const fb = gl.createFramebuffer(), rb = gl.createRenderbuffer()
+	gl.uniform1i(gl.getUniformLocation(p, 'last'), 1)
+	const res = new Uint32Array(512)
+	for(let i = 0; i < 256; i++) res[i<<1|1] = i
+	const fb = gl.createFramebuffer(), p1 = gl.createTexture(), p2 = gl.createTexture()
 	gl.bindFramebuffer(gl.FRAMEBUFFER, fb)
-	gl.bindRenderbuffer(gl.RENDERBUFFER, rb)
-	gl.renderbufferStorage(gl.RENDERBUFFER, gl.R32UI, 16, 16)
-	gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, rb)
+	gl.activeTexture(gl.TEXTURE1)
+	gl.bindTexture(gl.TEXTURE_2D, p1)
+	gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RG32UI, 16, 16)
+	gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, 16, 16, gl.RG_INTEGER, gl.UNSIGNED_INT, res)
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.bindTexture(gl.TEXTURE_2D, p2)
+	gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RG32UI, 16, 16)
+	gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, 16, 16, gl.RG_INTEGER, gl.UNSIGNED_INT, res)
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 	gl.viewport(0, 0, 16, 16)
-	console.debug('Captcha ready! %sms', (performance.now() - t0).toFixed(2))
-	const res = new Uint32Array(256)
+	document.body.textContent = `Captcha ready! (took ${(performance.now() - t0).toFixed(2)}ms)\n\n`
 	const t1 = performance.now()
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3)
-	gl.readPixels(0, 0, 16, 16, gl.RED_INTEGER, gl.UNSIGNED_INT, res)
+	const ROUNDS = 256
+	for(let i = 0; i < ROUNDS; i++){
+		gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, i&1?p2:p1, 0)
+		gl.bindTexture(gl.TEXTURE_2D, i&1?p1:p2)
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3)
+	}
+	gl.readPixels(0, 0, 16, 16, gl.RG_INTEGER, gl.UNSIGNED_INT, res)
 	let acc = 0
 	for(const x of res) acc += x
-	console.log('Captcha done in %sms\nhash: %s', performance.now() - t1, (acc>>>0).toString(16).padStart(8, '0'))
+	document.body.textContent += `Captcha completed in ${(performance.now() - t1).toFixed(2)}ms\nhash: ${(acc>>>0).toString(16).padStart(8, '0')}`
 }
 export default Captcha
